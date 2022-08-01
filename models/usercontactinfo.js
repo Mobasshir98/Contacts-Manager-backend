8b641801c86f3276c7e8a856d462ddf779604235
Mobasshir <mobasshirhasan20@gmail.com>
2022-08-01 20:34:29 +0530

    First-Commit

--- models/usercontactinfo.js
+++ models/usercontactinfo.js
@@ -1,14 +1,19 @@
 const mongoose= require('mongoose');
+
 const contactinfo=new mongoose.Schema({
     name:{type:String,required:true},
     designation:{type:String,required:true},
     company:{type:String,required:true},
     industry:{type:String,required:true},
     email:{type:String,required:true},
-    phonenumber:{type:Number,required:true},
+    phonenumber:{type:String,required:true},
     country:{type:String,required:true},
+})
+ 
+const usercontactinfo=new mongoose.Schema({
+    contacts:[contactinfo],
     user:{type:String,required:true}
 })
 
-const contact = mongoose.model('Contactinfo', contactinfo)
-module.exports=contact;
+const usercontact = mongoose.model("UserContactInfo", usercontactinfo)
+module.exports=usercontact;
